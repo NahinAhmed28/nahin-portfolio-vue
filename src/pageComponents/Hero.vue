e2ceddca7b1604300d10d0dba5c1b082775ead45
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-10-19 12:28:03 +0600

    type animation fixed

--- src/pageComponents/Hero.vue
+++ src/pageComponents/Hero.vue
@@ -9,9 +9,8 @@
         <div class="container">
           <!--<p class="display-6 color-d">Hello, world!</p>-->
 
-          <h1 class="hero-title mb-4">I am Nahin Ahmed</h1>
-          <p class="hero-subtitle"><span class="typed" data-typed-items="Designer, Developer, Photographer"></span></p>
-          <p class="pt-3"><a class="btn btn-secondary btn js-scroll px-4" href="#about" role="button">Learn More</a></p>
+
+
 
           <h1 class="hero-title mb-2">I am Nahin Ahmed</h1> <br>
 <!--          <p class="hero-subtitle"><span class="typed" data-typed-items="Designer, Developer, Photographer"></span></p>-->
@@ -21,7 +20,7 @@
             <span class="cursor" :class="{ typing: typeStatus }">&nbsp;</span>
           </p>
           <!-- <p class="pt-3"><a class="btn btn-primary btn js-scroll px-4" href="#about" role="button">Learn More</a></p> -->
-
+          <p class="pt-3"><a class="btn btn-secondary btn js-scroll px-4" href="#about" role="button">Learn More</a></p>
         </div>
       </div>
     </div>
