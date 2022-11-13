6613e06ea91e377b750fa7e15da28422de259052
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-11-13 17:49:11 +0600

    animation package added

--- src/pageComponents/About.vue
+++ src/pageComponents/About.vue
@@ -95,6 +95,60 @@
         </div>
       </div>
   </section>
+  <!-- ======= Testimonials Section ======= -->
+  <div class="testimonials paralax-mf bg-image" :style="{'background-image': 'url(/assets/img/overlay-bg.jpg)'}">
+    <div class="overlay-mf"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+
+          <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
+            <div class="swiper-wrapper">
+
+              <div class="swiper-slide">
+                <div class="testimonial-box">
+                  <div class="author-test">
+                    <div v-animate-onscroll.repeat="{down: 'animated zoomInUp'}">
+                      <img src="../../public/assets/img/testimonial-2.jpg" alt="" class="rounded-circle b-shadow-a">
+                    </div>
+                    <span class="author">Ahmed Nur-A- Jalal (Nahin)</span>
+                  </div>
+                  <div v-animate-onscroll.repeat="{down: 'animated slide-in-left'}">
+                    <div class="content-test">
+                      <p class="description lead">
+                        Junior Laravel and Vue.js web developer .I Have basic knowledge on android app development with native java and I am a data science enthusiast.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div><!-- End testimonial item -->
+
+              <div class="swiper-slide">
+                <div class="testimonial-box">
+                  <div class="author-test">
+                    <img src="../../public/assets/img/testimonial-4.jpg" alt="" class="rounded-circle b-shadow-a">
+                    <span class="author">Marta Socrate</span>
+                  </div>
+                  <div class="content-test">
+                    <p class="description lead">
+                      Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit.
+                    </p>
+                  </div>
+                </div>
+              </div><!-- End testimonial item -->
+            </div>
+            <div class="swiper-pagination"></div>
+          </div>
+
+          <!-- <div id="testimonial-mf" class="owl-carousel owl-theme">
+
+      </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- End Testimonials Section -->
   <!-- End About Section -->
 
 
