074785a6b5587f33370d0bf080a9dbc7af5a1ed9
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-11-14 13:06:35 +0600

    nav bar change continue

--- src/pageComponents/Header.vue
+++ src/pageComponents/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top" style="background-color: rgba(35,34,34,0.87)">
-    <div class="container d-flex align-items-center justify-content-between" >
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center justify-content-between">
 
       <h1 class="logo"><a href="index.html">DevFolio</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
@@ -13,7 +13,24 @@
           <li><a class="nav-link scrollto" href="#about">About</a></li>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
           <li><a class="nav-link scrollto " href="#work">Work</a></li>
-          <li><a class="nav-link scrollto " href="#blog">Blog</a></li>
+          <li><a class="nav-link scrollto " href="#blog">Courses</a></li>
+<!--          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>-->
+<!--            <ul>-->
+<!--              <li><a href="#">Drop Down 1</a></li>-->
+<!--              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>-->
+<!--                <ul>-->
+<!--                  <li><a href="#">Deep Drop Down 1</a></li>-->
+<!--                  <li><a href="#">Deep Drop Down 2</a></li>-->
+<!--                  <li><a href="#">Deep Drop Down 3</a></li>-->
+<!--                  <li><a href="#">Deep Drop Down 4</a></li>-->
+<!--                  <li><a href="#">Deep Drop Down 5</a></li>-->
+<!--                </ul>-->
+<!--              </li>-->
+<!--              <li><a href="#">Drop Down 2</a></li>-->
+<!--              <li><a href="#">Drop Down 3</a></li>-->
+<!--              <li><a href="#">Drop Down 4</a></li>-->
+<!--            </ul>-->
+<!--          </li>-->
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
