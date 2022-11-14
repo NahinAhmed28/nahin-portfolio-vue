7f63c6209b5a17480ee9581a54fde811618d6192
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-11-14 12:41:06 +0600

    nav bar change continue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import VueAnimateOnScroll from 'vue3-animate-onscroll';
+import 'bootstrap'
 
 createApp(App).use(VueAnimateOnScroll).mount('#app')
