1131ab499d838ef9a7b768c28763422967c863b1
NahinAhmed28 <Nahin.ahmed28@gmail.com>
2022-11-16 12:54:57 +0600

    about section modified

--- src/pageComponents/About.vue
+++ src/pageComponents/About.vue
@@ -45,6 +45,8 @@
                   </div>
                 </div>
               </div>
+
+
               <div class="col-md-6">
                   <div class="about-me pt-4 pt-md-0">
                     <div class="title-box-2">
